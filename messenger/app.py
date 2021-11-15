0b111edfbb2888ea61401bc09ed4cdfec79af59d
Kirill Inyakin <kirillinyakin@yandex.ru>
2021-11-15 15:19:24 +0300

    main file

--- messenger/app.py
+++ messenger/app.py
@@ -1,1 +1,23 @@
 from fastapi import APIRouter
+
+
+from routers import chat, messenge, user
+
+router = APIRouter(
+    prefix='/api/v1'
+)
+
+router.include_router(
+    chat.router,
+    tags=['Chat']
+)
+
+router.include_router(
+    messenge.router,
+    tags=['Messenge']
+)
+
+router.include_router(
+    user.router,
+    tags=['User']
+)
