4be8a307d9b05dda53ccd204e47bf5e8a5c72cfd
Kirill Inyakin <kirillinyakin@yandex.ru>
2021-11-28 23:36:28 +0300

    added function to create chat

--- messenger/routers/chat.py
+++ messenger/routers/chat.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
+from edgedb import Object
 
-from messenger.controllers.create_chat import create_chat
-from messenger.models.chat import CreateChatModel
+from messenger.controllers.chat import create_chat
+from messenger.models.chat import CreateChatModel, ResponseChatModel
 from messenger.service.enums import NotSuccessDetailChoices
 from messenger.service.utils import get_user_from_request, get_db_pool
 
@@ -11,12 +12,12 @@ router = APIRouter(
 )
 
 
-@router.post('/')
+@router.post('/', response_model=ResponseChatModel)
 async def create_chat_view(data: CreateChatModel, user=Depends(get_user_from_request), pool=Depends(get_db_pool)):
     if not user:
         raise HTTPException(
             status.HTTP_401_UNAUTHORIZED,
             NotSuccessDetailChoices.NOT_AUTHORISED
         )
-    result = await create_chat(data, user, pool)
+    result: Object = await create_chat(data, user, pool)
     return result
