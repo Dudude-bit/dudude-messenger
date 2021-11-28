99b7196ccb5b84099a2161f8476cdacdc9d8913d
Kirill Inyakin <kirillinyakin@yandex.ru>
2021-11-28 23:36:13 +0300

    function to create user and password recovery

--- messenger/routers/user.py
+++ messenger/routers/user.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response, status
+from edgedb import Object
 
-from messenger.controllers.create_user import create_user
-from messenger.models.user import CreateUserModel
+from messenger.controllers.user import create_user, recover_password
+from messenger.models.user import CreateUserModel, CreatePasswordRecoveryModel, ResponseUserModel
 from messenger.service.utils import get_db_pool
 
 router = APIRouter(
@@ -9,8 +10,16 @@ router = APIRouter(
 )
 
 
-@router.post('/', response_model=)
+@router.post('/', response_model=ResponseUserModel)
 async def create_user_view(data: CreateUserModel, pool=Depends(get_db_pool)):
-    result = await create_user(data, pool)
-
+    result: Object = await create_user(data, pool)
     return result
+
+
+@router.post('/password-recovery')
+async def password_recovery_view(data: CreatePasswordRecoveryModel, pool=Depends(get_db_pool)):
+    await recover_password(data, pool)
+
+    return Response(
+        status_code=status.HTTP_201_CREATED
+    )
