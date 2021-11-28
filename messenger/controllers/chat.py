37a72a885881ae0138acb1fa7100c55b735ddc09
Kirill Inyakin <kirillinyakin@yandex.ru>
2021-11-28 23:33:44 +0300

    function to create password recovery and create user completed

---
+++ messenger/controllers/chat.py
@@ -0,0 +1,11 @@
+from edgedb import AsyncIOClient
+
+from messenger.models.chat import CreateChatModel
+from messenger.service.utils import _create_chat
+
+
+async def create_chat(chat_data: CreateChatModel, user, pool: AsyncIOClient):
+    data = chat_data.dict()
+    data['user_id'] = user[0]['user']
+    result = (await _create_chat(data, pool))[0]
+    return result
