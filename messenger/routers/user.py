9184bff8d864260b8e186bdbec6d514c27e16f81
Kirill Inyakin <kirillinyakin@yandex.ru>
2021-11-28 21:06:41 +0300

    added route to create user

--- messenger/routers/user.py
+++ messenger/routers/user.py
@@ -1,5 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from messenger.controllers.create_user import create_user
+from messenger.models.user import CreateUserModel
+from messenger.service.utils import get_db_pool
 
 router = APIRouter(
     prefix='/user'
 )
+
+
+@router.post('/', response_model=)
+async def create_user_view(data: CreateUserModel, pool=Depends(get_db_pool)):
+    result = await create_user(data, pool)
+
+    return result
