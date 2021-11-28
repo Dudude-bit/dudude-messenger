6738c7ce0ce80bce85edb0e667f52fbd312863b1
Kirill Inyakin <kirillinyakin@yandex.ru>
2021-11-28 21:09:08 +0300

    added pydantic models of create user and chat request

---
+++ messenger/models/chat.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+from pydantic import BaseModel, validator, root_validator
+
+
+class ChatTypes(str, Enum):
+    PRIVATE = 'private'
+    GROUP = 'group'
+    CHANNEL = 'channel'
+
+
+class CreateChatModel(BaseModel):
+    type: ChatTypes
+    name: str = None
+
+    @root_validator
+    def validate_data(cls, values):
+        type = values.get('type')
+        name = values.get('name')
+
+        if type == ChatTypes.PRIVATE:
+            assert name is None, 'can\'t set name to a private chat'
+
+        return values
