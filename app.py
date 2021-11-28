0f4f49e79402134e51584e9d76daab5ba3f29a35
Kirill Inyakin <kirillinyakin@yandex.ru>
2021-11-28 23:52:14 +0300

    added connection to kafka and adding kafka to middleware

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from fastapi.responses import UJSONResponse
 from starlette.requests import Request
 
-from database import EdgeDatabase
+from database import EdgeDatabase, ExtendedKafkaProducer
 from exceptions import NotImproperlyConfigure
 import messenger
 
@@ -18,24 +18,34 @@ def create_app():
 
     edgedb_instance = os.getenv('edgedb_instance', None)
 
+    kafka_bootstrap_host = os.getenv('kafka_host', None)
+
     if edgedb_instance is None:
         raise NotImproperlyConfigure('define edgedb instance name')
 
+    if kafka_bootstrap_host is None:
+        raise NotImproperlyConfigure('define kafka host')
+
     db = EdgeDatabase(edgedb_instance)
+
+    kafka_queue = ExtendedKafkaProducer(bootstrap_servers=[kafka_bootstrap_host])
 
     @app.middleware('http')
     async def add_db_pool(request: Request, call_next: Callable):
         request.state.pool = db.pool
+        request.state.kafka = kafka_queue
         response = await call_next(request)
         return response
 
     @app.on_event('startup')
     async def on_startup_event():
         await db.create_pool()
+        await kafka_queue.check_for_connection()
 
     @app.on_event('shutdown')
     async def on_shutdown_event():
         await db.close_pool()
+        await kafka_queue.close_connection()
 
     return app
 
