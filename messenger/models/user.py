d76c5da7e8555c5a3174ec120a50e568321cdd2f
Kirill Inyakin <kirillinyakin@yandex.ru>
2021-11-28 23:36:43 +0300

    model of response create user

--- messenger/models/user.py
+++ messenger/models/user.py
@@ -4,3 +4,12 @@ from pydantic import BaseModel, EmailStr
 class CreateUserModel(BaseModel):
     username: str
     email: EmailStr
+
+
+class ResponseUserModel(BaseModel):
+    username: str
+    email: EmailStr
+
+
+class CreatePasswordRecoveryModel(BaseModel):
+    email: EmailStr
