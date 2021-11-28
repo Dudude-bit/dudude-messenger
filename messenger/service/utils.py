30177d6a272831c27d5c70de3c623270dc7a1404
Kirill Inyakin <kirillinyakin@yandex.ru>
2021-11-28 23:34:51 +0300

    completed function to create user, password_recovery

--- messenger/service/utils.py
+++ messenger/service/utils.py
@@ -22,17 +22,31 @@ async def _create_chat(chat_data, pool: AsyncIOClient):
 async def _create_user(user_data, pool: AsyncIOClient):
     query = """
     INSERT User {
-        created_at := <datetime>$created_at,
+        created_at := datetime_of_statement(),
         username := <str>$username,
         email := <email>$email,
-        activation_code := <str>$activation_code
+        activation_code := <uuid>$activation_code
     }
     """
-    result = pool.query(
+    result = await pool.query(
         query,
-        created_at=user_data['created_at'],
         username=user_data['username'],
         email=user_data['email'],
         activation_code=user_data['activation_code']
     )
     return result
+
+
+async def _create_password_recover(recover_data, pool: AsyncIOClient):
+    query = """
+    INSERT PasswordRecovery {
+        created_at := datetime_of_statement(),
+        expires := <duration>$expires,
+        token := <uuid>$token,
+        user := (select User filter .email = <email>$email)
+    }
+    """
+    await pool.query(query,
+                     expires=recover_data['expires'],
+                     token=recover_data['token'],
+                     email=recover_data['email'])
