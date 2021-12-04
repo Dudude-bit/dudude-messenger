7be523b9c48d5c3dd3eb532bbaa4fd352d99551d
Kirill Inyakin <kirillinyakin@yandex.ru>
2021-12-04 04:03:31 +0300

    added various models

--- messenger/models/user.py
+++ messenger/models/user.py
@@ -4,11 +4,21 @@ from pydantic import BaseModel, EmailStr
 class CreateUserModel(BaseModel):
     username: str
     email: EmailStr
+    password: str
 
 
 class ResponseUserModel(BaseModel):
     username: str
     email: EmailStr
+
+
+class LoginModel(BaseModel):
+    username: str
+    password: str
+
+
+class ResponseLoginModel(BaseModel):
+    value: str
 
 
 class CreatePasswordRecoveryModel(BaseModel):
