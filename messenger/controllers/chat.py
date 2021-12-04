6b4c6947b452f1e607ff98c16635da293d3c0233
Kirill Inyakin <kirillinyakin@yandex.ru>
2021-12-04 04:03:21 +0300

    refactor

--- messenger/controllers/chat.py
+++ messenger/controllers/chat.py
@@ -7,5 +7,4 @@ from messenger.service.utils import _create_chat
 async def create_chat(chat_data: CreateChatModel, user, pool: AsyncIOClient):
     data = chat_data.dict()
     data['user_id'] = user[0]['user']
-    result = (await _create_chat(data, pool))[0]
-    return result
+    return await _create_chat(data, pool)
--- messenger/controllers/user.py
+++ messenger/controllers/user.py
@@ -2,18 +2,47 @@ import datetime
 import uuid
 
 from edgedb import AsyncIOClient, MissingRequiredError
+from nats import NATS
+import ujson
 
-from messenger.models.user import CreateUserModel, CreatePasswordRecoveryModel
-from messenger.service.utils import _create_user, _create_password_recover
+from messenger.models.user import CreateUserModel, CreatePasswordRecoveryModel, LoginModel
+from messenger.service.utils import _create_user, _create_password_recover, _create_or_return_login_token
 
 
-async def create_user(user_data: CreateUserModel, pool: AsyncIOClient):
+async def create_user(user_data: CreateUserModel, pool: AsyncIOClient, nats_client: NATS):
     data = user_data.dict()
     data['activation_code'] = uuid.uuid4()
-    return (await _create_user(data, pool))[0]
+    result = await _create_user(data, pool)
+    query = """
+    SELECT User{username, email, activation_code} FILTER .id = <uuid>$id
+    """
+    result = await pool.query_required_single_json(query, id=result['id'])
+    await nats_client.publish('send-email-topic', ujson.dumps({
+        'email-payload': str(data['activation_code']),
+        'from-email': 'no-reply@dudude-bit.com',
+        'to-email': data['email'],
+        'email-subject': 'user-activation'
+    }).encode())
+    return ujson.loads(result)
 
 
-async def recover_password(recover_data: CreatePasswordRecoveryModel, pool: AsyncIOClient):
+async def activate_user(activation_code: uuid.UUID, pool: AsyncIOClient):
+    raise NotImplementedError()
+
+
+async def login(login_data: LoginModel, pool: AsyncIOClient):
+    data = login_data.dict()
+    data['value'] = uuid.uuid4()
+    result = await _create_or_return_login_token(data, pool)
+    query = """
+    SELECT Token{value} FILTER .id = <uuid>$id
+    """
+    result = await pool.query_required_single_json(query, id=result['id'])
+
+    return ujson.loads(result)
+
+
+async def recover_password(recover_data: CreatePasswordRecoveryModel, pool: AsyncIOClient, nats_client: NATS):
     data = recover_data.dict()
     data['expires'] = datetime.timedelta(hours=1)
     data['token'] = uuid.uuid4()
@@ -21,3 +50,10 @@ async def recover_password(recover_data: CreatePasswordRecoveryModel, pool: Asyn
         await _create_password_recover(data, pool)
     except MissingRequiredError:
         pass
+    else:
+        await nats_client.publish('send-email-topic', ujson.dumps({
+            'email-payload': str(data['token']),
+            'from-email': 'no-reply@dudude-bit.com',
+            'to-email': data['email'],
+            'email-subject': 'password-recovery'
+        }).encode())
