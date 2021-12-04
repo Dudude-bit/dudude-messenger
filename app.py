4dd00bb21ad22d2cb84d89018b04b61455426418
Kirill Inyakin <kirillinyakin@yandex.ru>
2021-12-04 04:03:07 +0300

    nats integration

--- app.py
+++ app.py
@@ -1,58 +1,64 @@
 import os
+import logging
 from typing import Callable
 
 from fastapi import FastAPI
 from fastapi.responses import UJSONResponse
 from starlette.requests import Request
 
-from database import EdgeDatabase, ExtendedKafkaProducer
+from database import EdgeDatabase, NatsQueue
 from exceptions import NotImproperlyConfigure
 import messenger
 
 
 def create_app():
-
     app = FastAPI(default_response_class=UJSONResponse)
 
     app.include_router(messenger.router)
 
     edgedb_instance = os.getenv('edgedb_instance', None)
 
-    kafka_bootstrap_host = os.getenv('kafka_host', None)
+    nats_dsn = os.getenv('nats_dsn', None)
 
     if edgedb_instance is None:
         raise NotImproperlyConfigure('define edgedb instance name')
 
-    if kafka_bootstrap_host is None:
-        raise NotImproperlyConfigure('define kafka host')
+    if nats_dsn is None:
+        raise NotImproperlyConfigure('define nats dsn')
 
     db = EdgeDatabase(edgedb_instance)
 
-    kafka_queue = ExtendedKafkaProducer(bootstrap_servers=[kafka_bootstrap_host])
+    nats_queue = NatsQueue(nats_dsn)
 
     @app.middleware('http')
     async def add_db_pool(request: Request, call_next: Callable):
         request.state.pool = db.pool
-        request.state.kafka = kafka_queue
+        request.state.nats = nats_queue.client
         response = await call_next(request)
         return response
 
     @app.on_event('startup')
     async def on_startup_event():
         await db.create_pool()
-        await kafka_queue.check_for_connection()
+        await nats_queue.connect()
 
     @app.on_event('shutdown')
     async def on_shutdown_event():
         await db.close_pool()
-        await kafka_queue.close_connection()
+        await nats_queue.close_connection()
 
     return app
 
 
 if __name__ == '__main__':
     import uvicorn
+
     app = create_app()
     uvicorn.run(
         app
     )
+
+# TODO add expiring logic to user token
+# TODO add hash function on password in user model
+# TODO think about not to query info again
+#  after creating instance at edgedb and get it from request json (not possible every time)
