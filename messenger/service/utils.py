6877d9b168d3d586ab6fdb41217cafd33d2e72ff
Kirill Inyakin <kirillinyakin@yandex.ru>
2021-12-04 04:02:00 +0300

    added function to create token, password recoverer

--- messenger/service/utils.py
+++ messenger/service/utils.py
@@ -1,3 +1,4 @@
+import ujson
 from edgedb import AsyncIOClient
 from fastapi import Header, Request
 
@@ -7,16 +8,16 @@ async def get_user_from_request(request: Request, token=Header(None)):
     query = """
     SELECT User {id} filter .token.value = <uuid>$token
     """
-    user = await pool.query(query, token=token)
-    return user
+    user = await pool.query_single_json(query, token=token)
+    return ujson.loads(user)
 
 
 def get_db_pool(request: Request):
     return request.state.pool
 
 
-def get_kafka(request: Request):
-    return request.state.kafka
+def get_nats_client(request: Request):
+    return request.state.nats
 
 
 async def _create_chat(chat_data, pool: AsyncIOClient):
@@ -29,16 +30,36 @@ async def _create_user(user_data, pool: AsyncIOClient):
         created_at := datetime_of_statement(),
         username := <str>$username,
         email := <email>$email,
+        password := <str>$password,
         activation_code := <uuid>$activation_code
     }
     """
-    result = await pool.query(
+    result = await pool.query_single_json(
         query,
         username=user_data['username'],
         email=user_data['email'],
+        password=user_data['password'],
         activation_code=user_data['activation_code']
     )
-    return result
+    return ujson.loads(result)
+
+
+async def _create_or_return_login_token(login_data, pool: AsyncIOClient):
+    query = """
+    INSERT Token {
+        created_at := datetime_of_statement(),
+        user := (select User filter .username = <str>$username and .password = <str>$password),
+        value := <uuid>$value
+    }
+    unless conflict on .user 
+    else (select Token)
+    """
+    result = await pool.query_single_json(query, username=login_data['username'],
+                                          password=login_data['password'],
+                                          value=login_data['value']
+                                          )
+
+    return ujson.loads(result)
 
 
 async def _create_password_recover(recover_data, pool: AsyncIOClient):
@@ -50,7 +71,7 @@ async def _create_password_recover(recover_data, pool: AsyncIOClient):
         user := (select User filter .email = <email>$email)
     }
     """
-    await pool.query(query,
-                     expires=recover_data['expires'],
-                     token=recover_data['token'],
-                     email=recover_data['email'])
+    await pool.query_single_json(query,
+                                 expires=recover_data['expires'],
+                                 token=recover_data['token'],
+                                 email=recover_data['email'])
