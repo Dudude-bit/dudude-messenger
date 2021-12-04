077459cde83e5bbb4a0fc74d9454e8933992d645
Kirill Inyakin <kirillinyakin@yandex.ru>
2021-12-04 04:02:19 +0300

    added routes to password recovere and login

--- messenger/routers/user.py
+++ messenger/routers/user.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, Depends, Response, status
-from edgedb import Object
+import uuid
 
-from messenger.controllers.user import create_user, recover_password
-from messenger.models.user import CreateUserModel, CreatePasswordRecoveryModel, ResponseUserModel
-from messenger.service.utils import get_db_pool
+from fastapi import APIRouter, Depends, Response, status
+
+from messenger.controllers.user import create_user, recover_password, login, activate_user
+from messenger.models.user import CreateUserModel, CreatePasswordRecoveryModel, ResponseUserModel, LoginModel, \
+    ResponseLoginModel
+from messenger.service.utils import get_db_pool, get_nats_client
 
 router = APIRouter(
     prefix='/user'
@@ -11,14 +13,33 @@ router = APIRouter(
 
 
 @router.post('/', response_model=ResponseUserModel)
-async def create_user_view(data: CreateUserModel, pool=Depends(get_db_pool)):
-    result: Object = await create_user(data, pool)
+async def create_user_view(data: CreateUserModel,
+                           pool=Depends(get_db_pool),
+                           nats_client=Depends(get_nats_client)):
+    result: dict = await create_user(data, pool, nats_client)
+    return result
+
+
+@router.post('/activate')
+async def activate_user_view(activation_code: uuid.UUID, pool=Depends(get_db_pool)):
+    await activate_user(activation_code, pool)
+    return Response(
+        status_code=status.HTTP_200_OK
+    )
+
+
+
+@router.post('/login', response_model=ResponseLoginModel)
+async def login_view(data: LoginModel, pool=Depends(get_db_pool)):
+    result: dict = await login(data, pool)
     return result
 
 
 @router.post('/password-recovery')
-async def password_recovery_view(data: CreatePasswordRecoveryModel, pool=Depends(get_db_pool)):
-    await recover_password(data, pool)
+async def password_recovery_view(data: CreatePasswordRecoveryModel,
+                                 pool=Depends(get_db_pool),
+                                 nats_client=Depends(get_nats_client)):
+    await recover_password(data, pool, nats_client)
 
     return Response(
         status_code=status.HTTP_201_CREATED
